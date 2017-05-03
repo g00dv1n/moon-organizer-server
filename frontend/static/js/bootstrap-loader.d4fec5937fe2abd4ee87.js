a32fadf7ffe099d44d361c0e20864281b0b70eea
Alexandr Brik <g00dv1n.private@gmail.com>
2017-05-03 22:50:15 +0300

    update frontend

---
+++ frontend/static/js/bootstrap-loader.d4fec5937fe2abd4ee87.js
@@ -0,0 +1,2 @@
+webpackJsonp([2],{205:function(t,s,n){t.exports=n.p+"static/fonts/glyphicons-halflings-regular.f4769f9.eot"},396:function(t,s,n){t.exports=n.p+"static/img/glyphicons-halflings-regular.89889688147bd7575d6327160d64e760.svg"},427:function(t,s,n){t.exports=n.p+"static/fonts/glyphicons-halflings-regular.e18bbf6.ttf"},428:function(t,s,n){t.exports=n.p+"static/fonts/glyphicons-halflings-regular.fa27723.woff"},429:function(t,s,n){t.exports=n.p+"static/fonts/glyphicons-halflings-regular.448c34a.woff2"}},[449]);
+//# sourceMappingURL=bootstrap-loader.d4fec5937fe2abd4ee87.js.map
