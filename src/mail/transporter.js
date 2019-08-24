bd25d9e1d8b65c9b9fe0a1eadbf3af98082667ec
g00dv1n <g00dv1n.private@gmail.com>
2019-08-24 16:36:28 +0300

    update mail conf

--- src/mail/transporter.js
+++ src/mail/transporter.js
@@ -2,7 +2,11 @@ import nodemailer from 'nodemailer'
 import config from 'config'
 
 const transporterFabric = () => {
-  const mailConfig = config.get('mail')
+  const mailConfig = Object.assign({}, config.get('mail'))
+  mailConfig.auth = {
+    user: process.env.SES_USER,
+    pass: process.env.SES_PWD
+  }
   return nodemailer.createTransport(
     Object.assign(mailConfig, {
       secure: true
